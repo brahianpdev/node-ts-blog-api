b7163c18e6dbb81a047a543d3ace29d857ae7b6b
brahianpdev <zk.spywn@gmail.com>
2021-12-21 21:56:04 -0300

    Add new .envs settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,12 @@ dotenv.config();
 
 const {
     MONGO_URL,
-    PORT_APP
+    PORT_APP,
+    SECRETOPRIVATEKEY,
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET,
+    CLOUDINARY_URL,
 } = process.env;
 
 export const config = {
@@ -12,5 +17,14 @@ export const config = {
     },
     app: {
         port: PORT_APP
+    },
+    jwt: {
+        key: SECRETOPRIVATEKEY
+    },
+    cloud: {
+        name: CLOUDINARY_CLOUD_NAME,
+        key: CLOUDINARY_API_KEY,
+        secret: CLOUDINARY_API_SECRET,
+        url: CLOUDINARY_URL
     }
 }
