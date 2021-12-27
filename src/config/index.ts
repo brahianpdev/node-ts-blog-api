f7f16455865f8f74e7772bf11c7e0fa24d144b3c
brahianpdev <zk.spywn@gmail.com>
2021-12-27 00:03:23 -0300

    update: add new's env

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,29 +2,35 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const {
-    MONGO_URL,
-    PORT_APP,
-    SECRETOPRIVATEKEY,
-    CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET,
-    CLOUDINARY_URL,
+	MONGO_URL,
+	PORT_APP,
+	SECRETOPRIVATEKEY,
+	CLOUDINARY_CLOUD_NAME,
+	CLOUDINARY_API_KEY,
+	CLOUDINARY_API_SECRET,
+	CLOUDINARY_URL,
+	GOOGLE_CLIENT_ID,
+	GOOGLE_SECRET_ID,
 } = process.env;
 
 export const config = {
-    db: {
-        mongo: MONGO_URL
-    },
-    app: {
-        port: PORT_APP
-    },
-    jwt: {
-        key: SECRETOPRIVATEKEY
-    },
-    cloud: {
-        name: CLOUDINARY_CLOUD_NAME,
-        key: CLOUDINARY_API_KEY,
-        secret: CLOUDINARY_API_SECRET,
-        url: CLOUDINARY_URL
-    }
-}
+	db: {
+		mongo: MONGO_URL,
+	},
+	app: {
+		port: PORT_APP,
+	},
+	jwt: {
+		key: SECRETOPRIVATEKEY,
+	},
+	cloud: {
+		name: CLOUDINARY_CLOUD_NAME,
+		key: CLOUDINARY_API_KEY,
+		secret: CLOUDINARY_API_SECRET,
+		url: CLOUDINARY_URL,
+	},
+	google: {
+		client: GOOGLE_CLIENT_ID,
+		secret: GOOGLE_SECRET_ID,
+	},
+};
