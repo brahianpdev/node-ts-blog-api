0a6678c3ad5c8622c7a8e0223b1deb8de37ba6d9
brahianpdev <zk.spywn@gmail.com>
2022-01-01 23:33:48 -0300

    Feat: Upload Middelware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,18 +1,5 @@
-import { validRole, validEmail, validUserByID, existsCategoryById, existsProductById } from './dbValidator';
-import { isAdmin, haveRole } from './validateRoles';
 import generateJWT from './generateJWT';
-import validateJWT from './validateJWT';
-import validatorFields from './validatorFields';
+export { generateJWT };
 
-export {
-	generateJWT,
-	validateJWT,
-	validatorFields,
-	validRole,
-	validEmail,
-	validUserByID,
-	existsCategoryById,
-	existsProductById,
-	isAdmin,
-	haveRole,
-};
+import multerUpload from './multerUpload';
+export { multerUpload };
